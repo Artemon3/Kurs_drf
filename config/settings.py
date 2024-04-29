fe016127bbe4707ccc73ce0a10e8832bb3505ac4
artem <levkin_mizuno@mail.ru>
2024-04-29 18:31:15 +0700

    KURS

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -158,13 +158,13 @@ CSRF_TRUSTED_ORIGINS = [
     "https://read-and-write.example.com",
 ]
 
-CACHES_LOCATION = 'redis://127.0.0.1:6379'
+CACHES_LOCATION = 'redis://redis:6379'
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Australia/Tasmania"
 
